896cca72249c204757b576b45ed74af39343bbf6
Koji Nakamaru <koji.nakamaru@gree.net>
2015-07-09 13:31:17 +0900

    fixed to link libgcc and libstdc++ statically.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,4 +4,5 @@ require 'mkmf'
 CONFIG['LDSHARED'] = "g++ " + CONFIG['LDSHARED'].split[1..-1].join(' ')
 
 #$defs.push("-D_NO_EXCEPTIONS")
+$LDFLAGS = '-static-libgcc -static-libstdc++'
 create_makefile("lzma_ruby")
