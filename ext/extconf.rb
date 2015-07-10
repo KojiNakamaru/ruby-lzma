021ca522bf4196a3990e06b5e40666356b6e8a5a
Koji Nakamaru <koji.nakamaru@gree.net>
2015-07-10 18:09:15 +0900

    fixed to check RUBY_PLATFORM for $LDFLAGS.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,5 +4,8 @@ require 'mkmf'
 CONFIG['LDSHARED'] = "g++ " + CONFIG['LDSHARED'].split[1..-1].join(' ')
 
 #$defs.push("-D_NO_EXCEPTIONS")
-$LDFLAGS = '-static-libgcc -static-libstdc++'
+case RUBY_PLATFORM
+when /mswin32|mingw/
+  $LDFLAGS = '-static-libgcc -static-libstdc++'
+end
 create_makefile("lzma_ruby")
